abc8c6c4457aa94a681944557bcf7b1f135fbb79
Marcel Ludwig <marcel.ludwig@avenga.com>
2022-06-30 11:56:14 +0200

    update router base path

--- _nuxt/static/1656581296/asdf/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/asdf", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1656582918/definitions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcouper-docs\u002F_nuxt\u002Fstatic\u002F1656582918",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdefinitions",config:{_app:{basePath:"\u002Fcouper-docs\u002F",assetsPath:"\u002Fcouper-docs\u002F_nuxt\u002F",cdnURL:a}}}}(null));
